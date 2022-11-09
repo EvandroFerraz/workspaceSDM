6982d915e067b499d51167c72dd10147aeb72956
evandro <ferraz.evandro@outlook.com>
2022-11-08 21:06:07 -0300

    08/11-fimAula

--- hospital/index.js
+++ hospital/index.js
@@ -1,51 +1,51 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.json())
 const porta = 3000
 const mysql = require('mysql2')
 
-app.get('/medicos', (req,res) => {
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'hospital',
-        password: '1234'
-    })
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE} = process.env
 
-    connection.query('SELECT * FROM tb_medico', (err, results, fields) => {
+const pool = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+app.get('/medicos', (req,res) => {
+    pool.query('SELECT * FROM tb_medico', (err, results, fields) => {
         console.log(results)
         res.json(results)
     })
 })
 
 app.get('/pacientes', (req,res) => {
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'hospital',
-        password: '1234'
-    })
-    connection.query('SELECT * FROM tb_paciente',  (err, results, fields) =>{
+    pool.query('SELECT * FROM tb_paciente',  (err, results, fields) =>{
         res.json(results)
     })
 })
 
 app.post('/medicos', (req, res) => {
-    const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'hospital',
-    password: '1234'
-    })
-
     const crm = req.body.crm
     const nome = req.body.nome
 
     const sql = "INSERT INTO tb_medico (crm, nome) VALUES (?, ?)"
-    connection.query(sql,[crm, nome], (err, results, fields) => {
+    pool.query(sql,[crm, nome], (err, results, fields) => {
     console.log (results)
      res.send('ok')
      })
+})
+
+app.get('/consultas', (req,res) => {
+    const sql = "SELECT m.nome as nome_medico, c.data_hora, p.nome as nome_paciente FROM tb_medico m, tb_consulta c, tb_paciente p WHERE m.crm = c.crm AND c.cpf = p.cpf"
+    pool.query(sql, (err, results, fields) => {
+        res.json(results)
+    })
 })
 
 app.listen(porta, () => console.log(`Executando. Porta ${porta}`))
